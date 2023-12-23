887fedef5f8c7b3e1c72ce7fe8b4465ec4156999
Ogshabzy23101 <ogshabzy23101@gmail.com>
2023-12-23 13:47:38 +0000

    dynamic display added to igbo section

--- app.js
+++ app.js
@@ -70,12 +70,77 @@ const yorubaFood = [
     category: `swallow`
   }
 ]
+const igboFood = [
+  {
+    img: `./images/igbo/abacha.jpeg`,
+    tittle: `abacha`,
+    cost: `#1500`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  {
+    img: `./images/igbo/egusi.jpeg`,
+    tittle: `egusi`,
+    cost: `#2500`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `swallow`
+  },
+  {
+    img: `./images/igbo/fio_fio.jpeg`,
+    tittle: `fio fio`,
+    cost: `#1000`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  {
+    img: `./images/igbo/nkwobi.jpeg`,
+    tittle: `nkwobi`,
+    cost: `#500`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  {
+    img: `./images/igbo/ogbono.jpeg`,
+    tittle: `ogbono`,
+    cost: `#1500`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `swallow`
+  },
+  {
+    img: `./images/igbo/okpa.jpeg`,
+    tittle: `okpa`,
+    cost: `#3500`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  {
+    img: `./images/igbo/ona.jpeg`,
+    tittle: `ona`,
+    cost: `#700`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  {
+    img: `./images/igbo/ukwa.jpeg`,
+    tittle: `ukwa`,
+    cost: `#2000`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  {
+    img: `./images/igbo/yam_vegetable.jpeg`,
+    tittle: `yam`,
+    cost: `#1500`,
+    desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo dolorem fugiat reiciendis culpa minima facilis quidem, ipsa,`,
+    category: `non swallow`
+  },
+  
+]
 const yoruba = document.querySelector('.yoruba-container')
 const igbo = document.querySelector('.igbo-container')
 const hausa = document.querySelector('.hausa-container')
 const other = document.querySelector('.other-container')
 const  general= document.querySelector('.general-container')
-
 
 
 function displayMenu(foodarr, foodcon) {
@@ -100,7 +165,7 @@ function displayMenu(foodarr, foodcon) {
 }
 
 
-window.addEventListener('DOMContentLoaded', displayMenu(yorubaFood,yoruba))
+window.addEventListener('DOMContentLoaded', displayMenu(yorubaFood,yoruba), displayMenu(igboFood,igbo))
 
 const secs = document.querySelectorAll('.sec')
 
